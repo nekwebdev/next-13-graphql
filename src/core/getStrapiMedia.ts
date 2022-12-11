f6a8bd92e80ba999264bd0a2f227a88b38e59133
nekwebdev <nek.webdev@gmail.com>
2022-12-10 22:12:58 -1000

    Cleanup getStrapiMedia

--- src/core/getStrapiMedia.ts
+++ src/core/getStrapiMedia.ts
@@ -1,10 +1,7 @@
-const mediaAPI = process.env.BASE_API_URL
-
 const getStrapiMedia = (url: string): string => {
-  // const { url } = media.data.attributes;
-  console.log(url)
-  const imageUrl = url?.startsWith('/') ? `${mediaAPI}${url}` : url
-  console.log(imageUrl)
+  const imageUrl = url?.startsWith('/')
+    ? `${process.env.BASE_API_URL}${url}`
+    : url
   return imageUrl
 }
 
