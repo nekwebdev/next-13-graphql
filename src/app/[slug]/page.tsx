8b180890ec018cfd3cf047773449bf9d6c762ed3
nekwebdev <nek.webdev@gmail.com>
2022-12-13 20:49:21 -1000

    Added a redirect example and more page styling

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,7 @@
 // GraphQL queries
 import getPageData from '@lib/getPageData'
 import Markdown from 'markdown-to-jsx'
+import { redirect } from 'next/navigation'
 
 // set props type
 type Props = {
@@ -11,14 +12,20 @@ type Props = {
 
 // use https://tailwindcss.com/docs/typography-plugin to style the markdown
 const Page = async ({ params }: Props) => {
+  params.slug == 'acceuil' ? redirect('/') : null
   const pageData = await getPageData(params.slug)
   return pageData ? (
-    <div className="flex justify-center">
-      <article className="prose lg:prose-xl prose-zinc dark:prose-invert">
-        <Markdown>
-          {pageData[0].attributes?.content ? pageData[0].attributes?.content : ''}
-        </Markdown>
-      </article>
+    <div className="flex flex-col mt-6">
+      <div className="text-4xl lg:text-6xl font-extrabold text-center">
+        <h1>{pageData[0].attributes?.title}</h1>
+      </div>
+      <div className="flex justify-center px-4">
+        <article className="prose lg:prose-xl prose-zinc dark:prose-invert justify-center">
+          <Markdown>
+            {pageData[0].attributes?.content ? pageData[0].attributes?.content : ''}
+          </Markdown>
+        </article>
+      </div>
     </div>
   ) : (
     <div />
