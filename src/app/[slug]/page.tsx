d490f81f05176c0a4002b39602972887555d5c79
nekwebdev <nek.webdev@gmail.com>
2022-12-13 21:42:54 -1000

    Now use redirects from getRedirectsData query

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,6 @@
 // GraphQL queries
 import getPageData from '@lib/getPageData'
+import getRedirectsData from '@lib/getRedirectsData'
 import Markdown from 'markdown-to-jsx'
 import { redirect } from 'next/navigation'
 
@@ -10,10 +11,20 @@ type Props = {
   }
 }
 
-// use https://tailwindcss.com/docs/typography-plugin to style the markdown
 const Page = async ({ params }: Props) => {
-  params.slug == 'acceuil' ? redirect('/') : null
+  // check for redirects before rendering the page for the given slug
+  const redirects = await getRedirectsData()
+  if (redirects) {
+    for (let index = 0; index < redirects.length; index++) {
+      params.slug == redirects[index].attributes?.source
+        ? redirect(redirects[index].attributes?.destination ?? '')
+        : null
+    }
+  }
+  // get our page data from the slug
   const pageData = await getPageData(params.slug)
+  // use https://github.com/probablyup/markdown-to-jsx &
+  // https://tailwindcss.com/docs/typography-plugin to style the markdown
   return pageData ? (
     <div className="flex flex-col mt-6">
       <div className="text-4xl lg:text-6xl font-extrabold text-center">
@@ -21,9 +32,7 @@ const Page = async ({ params }: Props) => {
       </div>
       <div className="flex justify-center px-4">
         <article className="prose lg:prose-xl prose-zinc dark:prose-invert justify-center">
-          <Markdown>
-            {pageData[0].attributes?.content ? pageData[0].attributes?.content : ''}
-          </Markdown>
+          <Markdown>{pageData[0].attributes?.content ?? ''}</Markdown>
         </article>
       </div>
     </div>
