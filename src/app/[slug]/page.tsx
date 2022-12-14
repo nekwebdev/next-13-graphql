4f5a52d274706888f17dc85c28a65488f6c3f180
nekwebdev <nek.webdev@gmail.com>
2022-12-13 14:29:06 -1000

    Add packages to help manage markdown content

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,6 @@
 // GraphQL queries
 import getPageData from '@lib/getPageData'
+import Markdown from 'markdown-to-jsx'
 
 // set props type
 type Props = {
@@ -8,9 +9,20 @@ type Props = {
   }
 }
 
+// use https://tailwindcss.com/docs/typography-plugin to style the markdown
 const Page = async ({ params }: Props) => {
   const pageData = await getPageData(params.slug)
-  return pageData ? <div>{pageData[0].attributes?.content}</div> : <div />
+  return pageData ? (
+    <div className="flex justify-center">
+      <article className="prose lg:prose-xl prose-zinc dark:prose-invert">
+        <Markdown>
+          {pageData[0].attributes?.content ? pageData[0].attributes?.content : ''}
+        </Markdown>
+      </article>
+    </div>
+  ) : (
+    <div />
+  )
 }
 
 export default Page
