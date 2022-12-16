3295b9f9082efd4c8d0ce16ed42011cf3d44dea1
nekwebdev <nek.webdev@gmail.com>
2022-12-15 22:40:46 -1000

    Cleaned up some type guards/predicates

--- src/lib/typePredicates.ts
+++ src/lib/typePredicates.ts
@@ -3,23 +3,24 @@ import {
   ComponentMenuDropdown,
   ComponentMenuLink,
   ComponentSharedSlide,
+  HomeSlider,
   Maybe,
   Navigation,
   NavigationLinksDynamicZone,
 } from '@lib/gql/graphql'
 
 // type predicates
-export const isLink = (entry: Maybe<NavigationLinksDynamicZone>): entry is ComponentMenuLink => {
-  return (entry as ComponentMenuLink).style !== undefined
-}
+export const isLink = (entry: Maybe<NavigationLinksDynamicZone>): entry is ComponentMenuLink =>
+  (entry as ComponentMenuLink).style !== undefined
 
 export const isDropdown = (
   entry: Maybe<NavigationLinksDynamicZone>
-): entry is ComponentMenuDropdown => {
-  return (entry as ComponentMenuDropdown).sections !== undefined
-}
+): entry is ComponentMenuDropdown => (entry as ComponentMenuDropdown).sections !== undefined
 
 export const isSlide = (entry: any): entry is ComponentSharedSlide =>
   (entry as ComponentSharedSlide).id !== undefined
+
+export const isHomeSlider = (entry: any): entry is HomeSlider =>
+  (entry as HomeSlider).slides !== undefined
 
 export const isNav = (entry: any): entry is Navigation => (entry as Navigation).logo !== undefined
